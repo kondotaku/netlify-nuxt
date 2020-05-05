a70c40c573413bf277a48b1711b56f1c37b7be22
近藤拓 <kondoutaku@kondoutakunoiMac.local>
2020-05-05 14:13:41 +0900

    form

--- pages/form.vue
+++ pages/form.vue
@@ -1,26 +1,24 @@
 <template>
   <div class>
     <client-only>
-      <form name="contact" method="POST" netlify>
+      <form
+        name="contact"
+        netlify-honeypot="bot-field"
+        action="thank-you"
+        data-netlify="true"
+        method="post"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <p class="hidden" style="display: none;">
+          <label>
+            Don’t fill this out:
+            <input name="bot-field" />
+          </label>
+        </p>
         <p>
           <label>
-            Your Name:
+            Email:
             <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email:
-            <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
           </label>
         </p>
         <p>
@@ -30,7 +28,7 @@
           </label>
         </p>
         <p>
-          <button type="submit">Send</button>
+          <button type="”submit”">Send</button>
         </p>
       </form>
     </client-only>
